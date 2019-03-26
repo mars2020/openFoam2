05f8f6bc4c48dda79aaebdc728cfe2c06004c167
Sungsoo Kim <rskim47@login3.stampede2.tacc.utexas.edu>
2019-03-26 18:45:13 -0500

    circle coordinates

--- MATLAB FILES/openfoam2.m
+++ MATLAB FILES/openfoam2.m
@@ -1,10 +1,19 @@
-t = [0:pi()/4:2*pi()]
-x = 0.5*cos(t);
-y = 0.5*sin(t);
+clc 
+clear all 
+close all 
 
-coord = zeros(9,2);
+t = [0:pi()/4:2*pi()];
+x = 1*cos(t);
+y = 1*sin(t);
+
+coord = zeros(9,3);
 coord(:,1) = x';
 coord(:,2) = y';
-coord
+coord;
 plot(x,y)
 grid on
+
+for i = 1 : 8 
+   fprintf('(%d %d %d)\n', coord(i,1),coord(i,2),coord(i,3)) 
+end
+
